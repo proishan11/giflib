f47a8c61211a4c79d9b064e2243002aea0d37b00
Ishan Singh <ankit.singh040@gmail.com>
2018-07-06 22:29:51 +0530

    first thymeleaf template

--- src/main/java/com/proishan11/giflib/controller/GifController.java
+++ src/main/java/com/proishan11/giflib/controller/GifController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GifController {
     @RequestMapping("/")
-    @ResponseBody
     public String listGifs() {
-        return "List of all the GIFs!";
+        return "home";
+    }
+
+    @RequestMapping("/gif")
+    @ResponseBody
+    public String showGif() {
+        return "This is a fucking GIF!";
     }
 }
