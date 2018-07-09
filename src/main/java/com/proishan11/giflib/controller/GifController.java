a794f54aef32766eef30f25aee43727cbc51c9f7
Ishan Singh <ankit.singh040@gmail.com>
2018-07-09 09:17:29 +0530

    feeding data to thymeleaf

--- src/main/java/com/proishan11/giflib/controller/GifController.java
+++ src/main/java/com/proishan11/giflib/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.proishan11.giflib.controller;
 
+import com.proishan11.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -12,8 +16,11 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String showGif() {
-        return "This is a fucking GIF!";
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("giphy", LocalDate.of(2015,2,13),
+                "Ishan Singh", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
+
 }
