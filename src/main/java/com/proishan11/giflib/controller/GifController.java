42fa4c6e06b76e1616c8eaa04a6c5e104f8d1fe5
Ishan Singh <ankit.singh040@gmail.com>
2018-07-09 11:30:05 +0530

    adds dynamic gif url

--- src/main/java/com/proishan11/giflib/controller/GifController.java
+++ src/main/java/com/proishan11/giflib/controller/GifController.java
@@ -5,6 +5,7 @@ import com.proishan11.giflib.model.Gif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -21,11 +22,17 @@ public class GifController {
         return "home";
     }
 
-    @RequestMapping("/gif")
-    public String gifDetails(ModelMap modelMap) {
-        Gif gif = gifRepository.findByName("penrose-triangle-illusion-gif");
+    // URL => http://localhost:8080/gif/penrose-triangle-illusion-gif
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
+    }
+
+    @RequestMapping("/categories")
+    public String categories(){
+        return "category";
     }
 
 }
